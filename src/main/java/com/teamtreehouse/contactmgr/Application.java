e1534f27995d78eaf8c8aeac20d176633afb923e
Saif Tase <saif.tase@sulzer-us.com>
2018-03-11 13:15:48 -0400

    Create methods to save and fetchAll contacts. Change hbm22ddl mode to update.

--- src/main/java/com/teamtreehouse/contactmgr/Application.java
+++ src/main/java/com/teamtreehouse/contactmgr/Application.java
@@ -2,11 +2,14 @@ package com.teamtreehouse.contactmgr;
 
 import com.teamtreehouse.contactmgr.model.Contact;
 import com.teamtreehouse.contactmgr.model.Contact.ContactBuilder;
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
 
 public class Application {
     // Hold a reusable reference to a SessionFactory (since we only need one)
@@ -22,7 +25,31 @@ public class Application {
                 .withEmail("saiftase@test.com")
                 .withPhone(1234567890L)
                 .build();
+        save(contact);
 
+        // Display a list of contacts
+        fetchAllContacts().stream().forEach(System.out::println);
+
+    }
+
+    @SuppressWarnings("unchecked")
+    private static List<Contact> fetchAllContacts(){
+        // Open a session
+        Session session = sessionFactory.openSession();
+
+        // Create Hibernate Criteria
+        Criteria criteria = session.createCriteria(Contact.class);
+
+        // Get a list of Contact objects according to the Criteria object
+        List<Contact> contacts = criteria.list();
+
+        // Close the session
+        session.close();
+
+        return contacts;
+    }
+
+    private static void save(Contact contact){
         // Open a session
         Session session = sessionFactory.openSession();
 
